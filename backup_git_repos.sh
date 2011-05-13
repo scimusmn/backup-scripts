56fd69925b3f57c630a6ec3609a86e25e5f443d2
bryan kennedy <bkennedy@smm.org>
2011-05-13 12:02:39 -0500

    Adding arguments
    First pass at creating the argument structure needed to make this script
    work in multiple situations.

--- backup_git_repos.sh
+++ backup_git_repos.sh
@@ -9,23 +9,66 @@
 # - Move it to the a folder for remote rsync'ing
 ############################################################
 
-repos=()
-backup_datetime=$(date +%Y_%m_%d_T%H_%M_%S)
-for repo in ${repos[@]}
-do
-  dest_dir=""
-  dest_file=$repo"_git_"$backup_datetime
-  dest=$dest_dir$dest_file
-  echo "==============================="
-  echo -e "CLONING "$repo"\r"
-  git clone --bare git@servername.domain$repo.git $dest
-  echo "==============================="
-  echo -e "COMPRESSING "$repo"\r"
-  tar vczf $dest.tgz -C $dest_dir $dest_file
-  rm -rf $dest
-  echo "==============================="
-  echo "SAVING to destination"
-  cp $dest.tgz /some/place
-  rm $dest.tgz
+TAR="$(which tar)"
+ECHO="$(which $ECHO)"
+GIT="$(which git)"
+
+# Usage description
+usage ()
+{
+cat << EOF
+USAGE: $0 arguments
+
+This script makes a compressed backup of all git repos in
+a central directory.
+
+ARGUMENTS:
+   ?   Display this help.
+
+  -s   Source directory, where your git repos are stored.
+  -d   Backup destination directory.
+  -u   Git username
+  -h   Git servername
+
+EOF
+}
+
+# Get the command line arguments.
+while getopts ":s:d:u:h:" opt ; do
+  case $opt in
+    s ) REPOS_SOURCE=$OPTARG ;;
+    d ) BACKUP_DEST=$OPTARG ;;
+    u ) GIT_USERNAME=$OPTARG ;;
+    h ) GIT_HOST=$OPTARG ;;
+
+    * ) $ECHO \n $usage
+      exit 1 ;;
+  esac
 done
 
+# Make sure all arguments are specified.
+if ( [ -z "$REPOS_SOURCE" ] || [ -z "$BACKUP_DEST" ] || [ -z "$GIT_USERNAME" ] || [ -z "$GIT_HOST" ] ) ; then
+  $ECHO ERROR: "You must supply all arguments."
+  usage
+  exit 1
+fi
+
+# Make sure the backup destination exits.
+if [ ! -d $BACKUP_DEST ] ; then
+  $ECHO ERROR: "The backup destination is not a valid directory."
+  usage
+  exit 1
+fi
+
+# Build array of git repos in the backup directory
+if [ -d "$REPOS_SOURCE" ]; then
+  for repo in $( find $REPOS_SOURCE -type f -name '*.git' )
+  do
+    $ECHO -e "CLONING "$repo"\r"
+    $GIT clone --bare $GIT_USERNAME@$GIT_HOST:$repo $BACKUP_DEST/$repo
+    $ECHO -e "COMPRESSING "$repo"\r"
+    $TAR vczf $BACKUP_DEST/$repo.tgz -C $BACKUP_DEST $repo
+    # Delete the cloned
+    # rm -rf $dest
+  done
+fi
