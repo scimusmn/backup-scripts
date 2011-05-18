4de06ac590479cd032914823ff65a37d1dfeddc1
bryan kennedy <bkennedy@smm.org>
2011-05-18 10:34:03 -0500

    Bugfix to delte multiple types of backups per run
    The script can now delete multiple types of backups per run. This fixes
    the bug where certain backups were getting left beheind because the
    script was only deleting the type of backup that was just created.

--- rsync_backup_tool.sh
+++ rsync_backup_tool.sh
@@ -15,8 +15,7 @@
 ############################################################
 
 # When should we make the daily backup - 24 hour time
-DAILY_TIME=10
-#DAILY_TIME=03
+DAILY_TIME=03
 # When should we make the weekly backup - Sunday(7)
 WEEKLY_DAY=6
 
@@ -164,8 +163,8 @@ WEEKLY_COUNT_IN_DAYS=$(($WEEKLY_COUNT*7))
 WEEKLY_DELETE_TIME=$(date -v-${WEEKLY_COUNT_IN_DAYS}d $DATE_STRING)
 MONTHLY_DELETE_TIME=$(date -v-${MONTHLY_COUNT}m $DATE_STRING)
 
-# Loop through all of the files and delete the matching files
-# declare -a files_to_delete
+# Loop through all of the files and populate an array of files to be deleted
+declare -a files_to_delete
 for backup in $DEST_PATH/* ; do
   if [ -f $backup ] ; then
     case "$backup" in
@@ -182,12 +181,12 @@ for backup in $DEST_PATH/* ; do
         files_to_delete=( "${files_to_delete[@]}" "$backup" )
         ;;
     esac
-    #rm -rf $backup
   fi
 done
+
+# Delete files
 for file_to_delete in "${files_to_delete[@]}" ; do
-  echo "********** deleting this file **********"
-  echo $file_to_delete
+  rm -rf $file_to_delete
   if [ $? = 0 ] ; then
     echo "Old snapshot deleted"
   else
@@ -195,6 +194,3 @@ for file_to_delete in "${files_to_delete[@]}" ; do
     echo "Unable to delete the old snapshot. Exiting." ; exit $?
   fi
 done
-echo "********** number of files to delete **********"
-echo ${#files_to_delete[@]}
-#echo "${files_to_delete[@]}"
